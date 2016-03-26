f3b009b9a3896713fbdc3ad8853f16ac50d6270a
chendongmidong <445397734@qq.com>
2016-03-27 00:32:06 +0800

    FriendList Activity UI

--- android/src/com/example/FoodieAndroidApp/activity/FriendList.java
+++ android/src/com/example/FoodieAndroidApp/activity/FriendList.java
@@ -2,22 +2,16 @@ package com.example.FoodieAndroidApp.activity;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.*;
+import android.widget.*;
 import com.example.FoodieAndroidApp.R;
 import com.example.FoodieAndroidApp.model.Friend;
 import com.example.FoodieAndroidApp.widge.FriendAdapter;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+import java.util.zip.Inflater;
 
 /**
  * Created by zhao on 2016/3/10.
@@ -28,6 +22,14 @@ public class FriendList extends Activity {
     private ListView friendsList;
     private FriendAdapter friendAdapter;
     private HashSet selectedSet;
+    private View mActionBarView ;
+    private Button confirmButton;
+    private LinearLayout galleryView;
+    private ImageView galleryImage;
+    private int avatorId = 0;
+    private HashMap avatorMap;
+    private Iterator iterator;
+    private List<Integer> positionList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ActionBar actionBar = getActionBar();
@@ -35,8 +37,15 @@ public class FriendList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friend_list);
 
+//        mActionBarView = LayoutInflater.from(this).inflate(R.layout.routine_action_bar,new RelativeLayout(this),false);
+//        confirmButton = (Button) mActionBarView.findViewById(R.id.confirm_button);
+//        actionBar.setCustomView(mActionBarView);
+
         initFriends();
         selectedSet = new HashSet();
+        avatorMap = new HashMap();
+        positionList = new ArrayList<Integer>();
+        galleryView = (LinearLayout)findViewById(R.id.gallery);
         friendsList = (ListView) findViewById(R.id.friends_list);
         friendsList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         friendAdapter = new FriendAdapter(this,R.layout.friend_item,friends,friendsList);
@@ -47,9 +56,25 @@ public class FriendList extends Activity {
                 friendAdapter.notifyDataSetChanged();
                 if(friendsList.isItemChecked(position)){
                     selectedSet.add(position);
+                    //avatorMap.put(avatorId,position);
+                    positionList.add(position);
                 }else {
                     selectedSet.remove(position);
+
+                    //完成在上面的scrollView动态显示头像功能
+                    for (int i= 0;i<positionList.size();i++){
+                        if (positionList.get(i) == position){
+                            while (positionList.get(i)!= null&& i<positionList.size()-1){
+                                positionList.set(i,positionList.get(i+1));
+                                i++;
+                            }
+                            positionList.remove(positionList.size()-1);
+                            break;
+                        }
+                    }
                 }
+                changeAvator();
+                avatorId++;
             }
         });
     }
@@ -71,6 +96,17 @@ public class FriendList extends Activity {
                 Toast.makeText(this,selectedSet.toString(),Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void changeAvator(){
+//        View view = LayoutInflater.from(FriendList.this).inflate(R.layout.gallery_item,null);
+        galleryView.removeAllViews();
+        for (int a:positionList){
+            View view1 = LayoutInflater.from(FriendList.this).inflate(R.layout.gallery_item,null);
+            ImageView imageView = (ImageView)view1.findViewById(R.id.gallery_image);
+            imageView.setImageResource(friends.get(a).getHeadPortrait());
+            galleryView.addView(view1);
+        }
     }
 
     private void initFriends() {
